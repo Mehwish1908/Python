69537231cca6b1eab1187c3e879c004ff75844fc
Mehwish Fathima <mehwish.f25@gcu.edu.in>
2026-05-07 15:18:28 +0530

    Delete Grade using user values.py

--- Grade using user values.py
+++
@@ -1,9 +0,0 @@
-9marks=input("Enter your marks: ")
-if marks>='90':
-    print("Grade is A")
-elif marks>='80':
-    print("Grade is B")
-elif marks>='70':
-    print("Grade is C")
-else:
-    print("Grade is D")
